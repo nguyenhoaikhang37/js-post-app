a63326eff260b0931824852a55b66ca0f2990a6d
Nguyen Hoai Khang <nguyenhoaikhang37@gmail.com>
2021-11-14 09:39:19 +0700

    Refactor
    - smart and dump func

--- js/home.js
+++ js/home.js
@@ -1,71 +1,5 @@
 import postApi from './apis/postApi';
-import { getUlPagination, setTextContent, truncateText } from './utils';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import debounce from 'lodash.debounce';
-
-dayjs.extend(relativeTime);
-
-function createPostElement(post) {
-  // clone template
-  const postTemplate = document.getElementById('postItemTemplate');
-  if (!postTemplate) return;
-
-  const liElement = postTemplate.content.firstElementChild.cloneNode(true);
-  if (!liElement) return;
-
-  // update title, desc, author, thumbnail
-  setTextContent(liElement, '[data-id="title"]', post.title);
-  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100));
-  setTextContent(liElement, '[data-id="author"]', post.author);
-  setTextContent(liElement, '[data-id="timeSpan"]', ` - ${dayjs(post.updatedAt).fromNow()}`);
-
-  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
-  if (thumbnailElement) {
-    thumbnailElement.src = post.imageUrl;
-
-    thumbnailElement.addEventListener('error', () => {
-      thumbnailElement.src = 'https://via.placeholder.com/1368x400?text=thumbnail';
-    });
-  }
-
-  return liElement;
-}
-
-function renderPostList(data) {
-  if (!Array.isArray(data)) return;
-
-  const ulElement = document.getElementById('postsList');
-  if (!ulElement) return;
-
-  // clear current ul
-  ulElement.textContent = '';
-
-  data.forEach((postElement, idx) => {
-    const liElement = createPostElement(postElement);
-    ulElement.appendChild(liElement);
-  });
-}
-
-function renderPagination(pagination) {
-  const ulPagination = getUlPagination();
-  if (!pagination || !ulPagination) return;
-
-  // calc total pages
-  const { _limit, _page, _totalRows } = pagination;
-  const totalPages = Math.ceil(_totalRows / _limit);
-
-  // save page and total pages to ulPagination
-  ulPagination.dataset.page = _page;
-  ulPagination.dataset.totalPages = totalPages;
-
-  // check enable/disable
-  if (_page <= 1) ulPagination.firstElementChild.classList.add('disabled');
-  else ulPagination.firstElementChild.classList.remove('disabled');
-
-  if (_page >= totalPages) ulPagination.lastElementChild.classList.add('disabled');
-  else ulPagination.lastElementChild.classList.remove('disabled');
-}
+import { initSearch, renderPagination, initPagination, renderPostList } from './utils';
 
 async function handleFilterChange(filterText, filterValue) {
   try {
@@ -80,84 +14,42 @@ async function handleFilterChange(filterText, filterValue) {
     // fetch API
     // re-render post list
     const { data, pagination } = await postApi.getAll(url.searchParams);
-    renderPostList(data);
-    renderPagination(pagination);
+    renderPostList('postsList', data);
+    renderPagination('pagination', pagination);
   } catch (error) {
     console.log('failed to fetch post list', error);
   }
 }
 
-function handlePrevClick(e) {
-  e.preventDefault();
-
-  const ulPagination = getUlPagination();
-  if (!ulPagination) return;
-
-  const page = parseInt(ulPagination.dataset.page) || 1;
-  if (!page) return;
-
-  if (page <= 1) return;
-
-  handleFilterChange('_page', page - 1);
-}
-
-function handleNextClick(e) {
-  e.preventDefault();
-  const ulPagination = getUlPagination();
-  if (!ulPagination) return;
-
-  const page = parseInt(ulPagination.dataset.page) || 1;
-  const totalPages = parseInt(ulPagination.dataset.totalPages);
-  if (!totalPages || !page) return;
-
-  if (page >= totalPages) return;
-
-  handleFilterChange('_page', page + 1);
-}
-
-function initPagination() {
-  const ulPagination = getUlPagination();
-  if (!ulPagination) return;
-
-  const prevLink = ulPagination.firstElementChild.firstElementChild;
-  if (prevLink) prevLink.addEventListener('click', handlePrevClick);
-
-  const nextLink = ulPagination.lastElementChild.firstElementChild;
-  if (nextLink) nextLink.addEventListener('click', handleNextClick);
-}
-
-function getDefaultParams() {
-  const url = new URL(window.location);
-
-  // update search params if needed
-  if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1);
-  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6);
-
-  history.pushState({}, '', url);
-  return url.searchParams;
-}
-
-function initSearch() {
-  const searchInput = document.getElementById('searchInput');
-  if (!searchInput) return;
-
-  const queryParams = new URLSearchParams(window.location.search);
-  if (queryParams.get('title_like')) {
-    searchInput.value = queryParams.get('title_like');
-  }
-
-  const debounceSearch = debounce((e) => handleFilterChange('title_like', e.target.value), 500);
-  searchInput.addEventListener('input', debounceSearch);
-}
-
+// Main
 (async () => {
-  initPagination();
-  initSearch();
+  try {
+    const url = new URL(window.location);
 
-  const queryParams = getDefaultParams();
+    // update search params if needed
+    if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1);
+    if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6);
 
-  // const queryParams = new URLSearchParams(window.location.search);
-  const { data, pagination } = await postApi.getAll(queryParams);
-  renderPostList(data);
-  renderPagination(pagination);
+    history.pushState({}, '', url);
+    const queryParams = url.searchParams;
+
+    initPagination({
+      elementId: 'pagination',
+      defaultParams: queryParams,
+      onChange: (page) => handleFilterChange('_page', page),
+    });
+
+    initSearch({
+      elementId: 'searchInput',
+      defaultParams: queryParams,
+      onChange: (value) => handleFilterChange('title_like', value),
+    });
+
+    // const queryParams = new URLSearchParams(window.location.search);
+    const { data, pagination } = await postApi.getAll(queryParams);
+    renderPostList('postsList', data);
+    renderPagination('pagination', pagination);
+  } catch (error) {
+    console.log('get all failed', error);
+  }
 })();
---
+++ js/utils/post.js
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { setTextContent, truncateText } from './common';
+
+dayjs.extend(relativeTime);
+
+export function createPostElement(post) {
+  // clone template
+  const postTemplate = document.getElementById('postItemTemplate');
+  if (!postTemplate) return;
+
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+  if (!liElement) return;
+
+  // update title, desc, author, thumbnail
+  setTextContent(liElement, '[data-id="title"]', post.title);
+  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100));
+  setTextContent(liElement, '[data-id="author"]', post.author);
+  setTextContent(liElement, '[data-id="timeSpan"]', ` - ${dayjs(post.updatedAt).fromNow()}`);
+
+  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
+  if (thumbnailElement) {
+    thumbnailElement.src = post.imageUrl;
+
+    thumbnailElement.addEventListener('error', () => {
+      thumbnailElement.src = 'https://via.placeholder.com/1368x400?text=thumbnail';
+    });
+  }
+
+  return liElement;
+}
+
+export function renderPostList(elementId, data) {
+  if (!Array.isArray(data)) return;
+
+  const ulElement = document.getElementById(elementId);
+  if (!ulElement) return;
+
+  // clear current ul
+  ulElement.textContent = '';
+
+  data.forEach((postElement) => {
+    const liElement = createPostElement(postElement);
+    ulElement.appendChild(liElement);
+  });
+}
